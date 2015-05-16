d21050c5edd9040533cbd2fa140260d6adb71657
Takuma Maruyama <maruyama@mfac.jp>
2015-05-16 17:31:00 +0900

    position created

---
+++ Assets/Resources/Scripts/PlayerInputBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class PlayerInputBehaviour : MonoBehaviour {
+	public GameObject ballObject;
+
+	// Use this for initialization
+	void Start () {
+		// this.OnMouseDownAsObservable ();
+		Observable.EveryUpdate ()
+			.Where (_ => Input.GetMouseButtonDown(0))
+				.Select (_ => Input.mousePosition)
+				.Subscribe (position => {
+					position = Camera.main.ScreenToWorldPoint(position);
+					Debug.Log ("touch:" + position.x + "," + position.y);
+					this.ballObject.GetComponent<Rigidbody2D>().position = new Vector2(position.x, position.y);
+				});
+	}
+}
---
+++ Assets/Resources/Scripts/SpringBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class SpringBehaviour : MonoBehaviour {
+	private SpringJoint2D currentJoint;
+
+	// Use this for initialization
+	void Start () {
+		currentJoint = this.gameObject.GetComponent<SpringJoint2D> ();
+
+		// when collision enter 2d for current joint. disable
+		this.OnTriggerEnter2DAsObservable ()
+			// XXX: must filter without self
+			// .Select (collision => collision.gameObject.GetComponent<Rigidbody2D>() == currentJoint.connectedBody)
+			.Where (collider => collider.gameObject.tag == "Joint")
+			.Subscribe (collider => {
+					if (this.currentJoint.enabled) {
+						this.currentJoint.enabled = false;
+					}
+					else {
+						this.currentJoint.enabled = true;
+						this.currentJoint.connectedBody = collider.gameObject.GetComponent<Rigidbody2D>();
+					}
+		});
+	}
+
+	void OnCollisionEnter2D() {
+		Debug.Log ("collision");
+	}
+
+	void OnTriggerEnter2D() {
+		Debug.Log ("trigger");
+	}
+}
