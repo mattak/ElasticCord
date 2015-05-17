63181ea5ec3d76ce5bd2c9ef4484e1e434e8b9f9
mattak <mattak.me@gmail.com>
2015-05-18 01:11:35 +0900

    warping only when boll releasing

--- Assets/Resources/Scripts/WarpInBehaviour.cs
+++ Assets/Resources/Scripts/WarpInBehaviour.cs
@@ -10,10 +10,9 @@ public class WarpInBehaviour : MonoBehaviour {
 	void Start () {
 		this.gameObject.OnTriggerEnter2DAsObservable ()
 			.Where (collider => collider.gameObject.tag == "Ball")
-			.Select (collider => collider.gameObject.GetComponent<BallBehaviour>())
-			//.Where (ball => !ball.GetIsWarping())
+			.Select (collider => collider.gameObject.GetComponent<SpringBehaviour>())
+			.Where (ball => !ball.IsJointing())
 			.Subscribe (ball => {
-					ball.SetIsWarping (true);
 					Vector3 warpPosition = warpToObject.transform.position;
 					ball.gameObject.transform.position = new Vector3 (warpPosition.x, warpPosition.y, ball.gameObject.transform.position.z);					                                                   
 			});
